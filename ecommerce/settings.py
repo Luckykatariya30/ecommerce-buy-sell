34e49a9df602241f36dd39f1c0f04ddbb0a71028
Luckykatariya30 <122107150+Luckykatariya30@users.noreply.github.com>
2026-01-16 10:29:05 +0530

    email & pass remore
    Updated email settings to use placeholder values for user and password.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'luckykatariya30@gmail.com'
-EMAIL_HOST_PASSWORD = 'kyuyqlubedwjwmbe'
+EMAIL_HOST_USER = 'your email ID'
+EMAIL_HOST_PASSWORD = 'email password'
 EMAIL_PORT = 587
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
